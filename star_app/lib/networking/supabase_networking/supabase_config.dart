6e6887f7b29a482a7756f5a235676656a82e1caf
Ruba Hilal <developerruba@outlook.com>
2023-11-30 12:39:34 +0300

    -

--- star_app/lib/networking/supabase_networking/supabase_config.dart
+++ star_app/lib/networking/supabase_networking/supabase_config.dart
@@ -7,5 +7,5 @@ supabaseConfig() async {
   await Supabase.initialize(
       url: "https://zkidhsvhkjshockssdec.supabase.co",
       anonKey:
-          "3MiOiJzdXBhYmFzZSIsInJlZiI6InpraWRoc3Zoa2pzaG9ja3NzZGVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTk3NzcyMTQsImV4cCI6MjAxNTM1MzIxNH0.3AqZboq5v9C35jZX5IY1qdR8Xve6gHXQn5HaGFwOuW8");
+          ".eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpraWRoc3Zoa2pzaG9ja3NzZGVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTk3NzcyMTQsImV4cCI6MjAxNTM1MzIxNH0.3AqZboq5v9C35jZX5IY1qdR8Xve6gHXQn5HaGFwOuW8");
 }
